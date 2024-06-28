3db1e74953d894b67af4179ddc1be2b47752c8ab
Sura <suri.murodova@gmail.com>
2024-06-28 14:22:43 +0300

    Add password constraints

--- app/api/schemas/user.py
+++ app/api/schemas/user.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr
 
 
 class User(BaseModel):
     username: str
-    password: str
+    password: constr(min_length=8, max_length=16)
